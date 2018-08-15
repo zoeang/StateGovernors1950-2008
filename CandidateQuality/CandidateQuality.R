9af851b74abb7cc2aff8430ca9733323e970c7a2
zoeang <36051346+zoeang@users.noreply.github.com>
2018-08-15 09:59:16 -0500

    See GovDatFinal.csv

--- CandidateQuality/CandidateQuality.R
+++ CandidateQuality/CandidateQuality.R
@@ -11,23 +11,32 @@ qual$name<-toupper(qual$name)
 #ref code
 #subset both winner and runner up--------------------------------------------------------
 
-
 #----------------------------
-library(readstata13)
+library(dplyr)
 cand<- read.dta13('C:/Users/zoeja/OneDrive/Documents/Summer 2018/GovernorData/StateGovernors1950-2008/Elections/replication/Stata/election1.dta')
 names(cand)
 
-cand2<- cand %>% 
+cand2<- cand %>% #get df of just the winner and runner up
   group_by(state, year) %>% 
   top_n(2, wt=vote) 
-cand2$candidate<-toupper(cand2$candidate)
-cand2$party1<-cand2$party
-cand2$party<-substr(cand2$party1,1,1)
 
+cand2$party1<-cand2$party#there was a reason for this, but I don't remember
+cand2$party<-substr(cand2$party1,1,1) # get the first letter for merging
 
-
-test1<-merge(qual, cand2, by=c('state', 'year', 'party'), all.x = T)
-test1<-test1[,c(1:5, 38, 6:37, 39:44)]
-testcheck<-test1[c(155, 157, 178, 181, 192, 555, 648, 653, 657,658, 662, 697,698, 753),]
-#155, 157, : wrong, clean out
+test1<-merge(qual, cand2, by=c('state', 'year', 'party'), all.x = T) #merge candidate votes and candidate quality
+test1<-test1[,c(1:5, 39, 6:38, 40:44)] #rearrange rows so candidate name strings are next to one another
+testcheck<-test1[c(155, 157, 178, 181, 192, 555, 648, 653, 657,658, 662, 697,698, 753),] #df of strings that do not "match"
+#155, 157, 192,555,648, 653, 657,658, 662, 753, : wrong, clean out
 #178, 181: correct, runner up
+#switch 697,698
+
+
+test1[c(155, 157, 192,555,648, 653, 657,658, 662, 753), c(6,39:44)]<-NA #clean out things that were wrong (candidates did not match and there wasn't a matching candidate for the row), replace with NA
+test1$winner<-ifelse(test1$w_g==1,1,0) #code for runner up, basically the same as w_g, but maintains the original col
+test1$winner[c(178,181)]<-0
+save697<-test1[697,]# save this info so I can switch inputed cand vars 
+test1[697, c(6,39:44)]<-test1[698, c(6,39:44)] #save 698 into 697
+test1[698, c(6,39:44)]<-save697[,c(6,39:44)]#save 697 into 698
+#if winner==1,
+setwd('C:/Users/zoeja/OneDrive/Documents/Summer 2018/GovernorData/StateGovernors1950-2008/CandidateQuality')
+write.csv(test1, 'candqualtop2.csv')
---
+++ DataCleaning/CandQualMerge.R
@@ -0,0 +1,22 @@
+dat<-read.csv("C:/Users/zoeja/OneDrive/Documents/Summer 2018/GovernorData/StateGovernors1950-2008/GovDat.csv")
+
+
+candqual<-read.csv('C:/Users/zoeja/OneDrive/Documents/Summer 2018/GovernorData/StateGovernors1950-2008/CandidateQuality/candqualtop2.csv')
+names(candqual)[2]<-'st'#relabel state as st for merge
+candqual$gov_party<-ifelse(candqual$party=="D", 1, 0)# recode to match dat
+candqual$year1<-candqual$year#save as different col so I don't have to change col names and orders, which is annoying 
+candqual$year<-candqual$year1+1 #add 1 to the year because candqual lists years as the year a candidate ran for office and datall terms starts when a gov takes office (the calendar year after winning election)
+candqual1<-candqual[,c('year', 'st', 'winner', 'gov_party', 'name', 'number_endorsements', 'number_cands')]#subset relevant cols
+dat1<-merge(dat, candqual1, by=c('st', 'year', 'gov_party'), all.x = T)
+
+#rearrange cols to check that names match 
+dat1<-dat1[,c(1:3, 5,42, 41, 6:40, 43:44)]
+
+dat1$winner_endorsements<-ifelse(dat1$winner==1, dat1$number_endorsements, NA) #if the candqual candidate was the winner, save their endorsements as winner_endorsements
+dat1$winner_primary_candidates<-ifelse(dat1$winner==1, dat1$number_cands, NA) # is the candqual candidate was the winner, save the number of candidates in their primary election as wnner_primary_candidates
+
+dat1$runnerup_endorsements<-ifelse(is.na(dat1$winner), dat1$number_endorsements, NA) #if candqual candidate was the runnnerup, same process as ^^^
+dat1$runnerup_primary_candidates<-ifelse(is.na(dat1$winner==0), dat1$number_cands, NA)
+
+dat2<-dat1[,-c('name', 'winner', 'number_endorsements', 'number_cands')] #remove columns  ##r didn't like this
+dat2<-dat1[,-c(5,6,42,43)]#same thing with numbers 
